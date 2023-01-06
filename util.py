17e7afa593667206c1fa51ef513d1b1cd8fa4bac
mitchwalker12 <93282442+mitchwalker12@users.noreply.github.com>
2023-01-06 12:31:31 -0700

    updated documentation

--- util.py
+++ util.py
@@ -1,10 +1,13 @@
-def nth_power(n, power):
+def nth_power(n, power=2):
     '''
     calculates power for number up to n
+    args:
+        n: highest number in the list of numbers
+        power: power for number to raise, default power is 2
     '''
     if power <= 10:
         return [i**power for i in range(n)]
     else:
         return print("You've exceeded the upper limit")
 
-print(nth_power(10, 4))
+print(nth_power(10))
