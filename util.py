64fd54c9a39c6eed718ba5fa1b1f2951592a6d05
Mitchell Walker <mitchell.walker@comcast.net>
2023-01-06 10:55:55 -0700

    updated upper limit documentation

--- util.py
+++ util.py
@@ -2,6 +2,9 @@ def nth_power(n, power):
     '''
     calculates power for number up to n
     '''
-    return [i**power for i in range(n)]
+    if power < = 10:
+        return [i**power for i in range(n)]
+    else:
+        return print("You've exceeded the upper limit")
 
 print(nth_power(10, 4))
